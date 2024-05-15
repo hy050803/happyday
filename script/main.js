c12feedae1228524e1205c0a6e3a76ba0142e187
韩宇 <@LAPTOP-C2IIDQSL>
2024-05-15 20:54:52 +0800

    7

--- script/main.js
+++ script/main.js
@@ -163,8 +163,6 @@ const animationTimeline = () => {
     )
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
